da9a79b72118f451b64c83f64e5cf79723ed0d98
javierlg91 <javilogut91@gmail.com>
2022-10-31 02:07:00 +0000

    CSS, refactoring and improvents

--- Hunt-The-Wumpus/src/app/components/campo-de-batalla/campo-de-batalla.component.ts
+++ Hunt-The-Wumpus/src/app/components/campo-de-batalla/campo-de-batalla.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 import { empty } from 'rxjs';
 
 @Component({
@@ -9,45 +10,54 @@ import { empty } from 'rxjs';
 export class CampoDeBatallaComponent implements OnInit {
   columnas = 5;
   filas = this.columnas;
-  monsters = 2;
+  monsters = 1;
+  arrows = 5;
   holes = 3;
   finish = false;
   win = false;
+  ingotMessage = false;
   gamePlay = false;
   gameMenu = true;
   playerMessage = false;
   lingoteAtrapado = false;
-  rightRotation = 0;
-  leftRotation = 0;
-  imagen = document.querySelector('#player');
-  message = document.querySelectorAll('#player-message');
+  rotation: any = 0;
+  monsterMessage = false;
+  arrowMessage = false;
 
   tablero = Array(this.filas)
     .fill('')
     .map(() => Array(this.columnas).fill('empty'));
+
   posicionJugador: any = {
     fila: 0,
     columna: 0,
   };
 
-  constructor() {}
-  ngOnInit(): void {
-    this.message = this.message;
-    console.log(this.tablero);
-    console.log('mensaje', this.message);
-  }
+  posicionMonster: any = {
+    fila: 0,
+    columna: 0,
+  };
 
+  posicionLingote: any = {
+    fila: 0,
+    columna: 0,
+  };
+  constructor() {}
+  ngOnInit(): void {}
+
+  /* reset the table and his values,generating random positions */
   public reset() {
-    console.log('imagen', this.imagen);
+    this.playerMessage = false;
     this.gameMenu = false;
     this.gamePlay = true;
     this.finish = false;
+    this.rotation = 0;
+    this.win = false;
     this.filas = this.columnas;
     this.tablero = Array(this.filas)
       .fill('')
       .map(() => Array(this.columnas).fill('empty'));
     this.lingoteAtrapado = false;
-    console.log('lingote inicio ', this.lingoteAtrapado);
 
     this.tablero[this.filas - 1][0] = 'player';
 
@@ -55,34 +65,30 @@ export class CampoDeBatallaComponent implements OnInit {
       const position = this.getRandomPosition();
       this.tablero[position.fila][position.columna] = 'lingote';
     }
+    this.getLingote();
 
     for (let i = 0; i < this.monsters; i++) {
       const position = this.getRandomPosition();
       this.tablero[position.fila][position.columna] = 'monster';
     }
+
     for (let i = 0; i < this.holes; i++) {
       const position = this.getRandomPosition();
       this.tablero[position.fila][position.columna] = 'hole';
     }
 
-    console.log('tablero despues de reseteo', this.tablero);
-    console.log(this.getPlayer());
+    this.getPlayer();
+    this.getMonster();
   }
 
-  public rotationToRight() {
-    this.rightRotation = +1;
-    console.log('rotationrigth', this.rightRotation);
-  }
-
-  public rotationToLeft() {
-    this.leftRotation = +1;
-    console.log('rotationrigth', this.leftRotation);
-  }
-
-  /* Función para desplazar el jugador hacia abajo */
+  /* Control function for the player movements*/
   private moveTo(fila: number, columna: number) {
-    /* Le restamos 1 posición */
     if (this.tablero[fila][columna] === 'empty') {
+      /* all the variables set to initial value */
+      this.ingotMessage = false;
+      this.playerMessage = false;
+      this.monsterMessage = false;
+      this.arrowMessage = false;
       this.tablero[this.posicionJugador.fila][this.posicionJugador.columna] =
         'empty';
       this.tablero[fila][columna] = 'player';
@@ -92,44 +98,123 @@ export class CampoDeBatallaComponent implements OnInit {
       this.tablero[this.posicionJugador.fila][this.posicionJugador.columna] =
         'empty';
       this.lingoteAtrapado = true;
-      console.log('Lingote', this.lingoteAtrapado);
-    } else if (
-      this.lingoteAtrapado == true &&
-      this.tablero[this.filas - 1][0] == 'player'
-    ) {
-      console.log(this.lingoteAtrapado);
-      this.getPlayer();
-      this.gamePlay = false;
-      this.finish = true;
-      this.win = true;
+      this.ingotMessage = true;
     } else if (
       this.tablero[fila][columna] === 'monster' ||
       this.tablero[fila][columna] === 'hole'
     ) {
       this.gamePlay = false;
       this.finish = true;
-      this.win = false;
     }
+    /* Check theese two conditions to win the game */
+    if (
+      this.lingoteAtrapado === true &&
+      this.tablero[this.filas - 1][0] === 'player'
+    ) {
+      this.gamePlay = false;
+      this.finish = true;
+      this.win = true;
+    }
+    /* Shows an advice when the monster is close */
+    if (
+      this.tablero[this.posicionMonster.fila][
+        this.posicionMonster.columna + 1
+      ] === 'player' ||
+      this.tablero[this.posicionMonster.fila][
+        this.posicionMonster.columna - 1
+      ] === 'player'
+    ) {
+      this.playerMessage = true;
+    }
+    this.getMonster();
     this.getPlayer();
-
-    console.log('tablero..', this.tablero);
   }
 
+  public posicion() {
+    return 'transform: rotate(' + this.rotation + 'deg)';
+  }
+  /* Functions to set the player directions and rotations*/
+
   public moveUp() {
-    this.moveTo(this.posicionJugador.fila - 1, this.posicionJugador.columna);
+    if (this.rotation === -90) {
+      this.moveTo(this.posicionJugador.fila - 1, this.posicionJugador.columna);
+    } else {
+      this.rotation = -90;
+    }
   }
 
   public moveDown() {
-    this.moveTo(this.posicionJugador.fila + 1, this.posicionJugador.columna);
+    if (this.rotation === 90) {
+      this.moveTo(this.posicionJugador.fila + 1, this.posicionJugador.columna);
+    } else {
+      this.rotation = 90;
+    }
   }
 
   public moveLeft() {
-    this.moveTo(this.posicionJugador.fila, this.posicionJugador.columna - 1);
+    if (this.rotation === -180) {
+      this.moveTo(this.posicionJugador.fila, this.posicionJugador.columna - 1);
+    } else {
+      this.rotation = -180;
+    }
   }
 
   public moveRight() {
-    this.moveTo(this.posicionJugador.fila, this.posicionJugador.columna + 1);
+    if (this.rotation === 0) {
+      this.moveTo(this.posicionJugador.fila, this.posicionJugador.columna + 1);
+    } else {
+      this.rotation = 0;
+    }
   }
+
+  /* if this 2 conditions are true, the monster will die */
+
+  public throwArrow() {
+    if (this.arrows > 0) {
+      if (
+        this.rotation === 0 &&
+        this.posicionMonster.fila === this.posicionJugador.fila &&
+        this.posicionMonster.columna > this.posicionJugador.columna
+      ) {
+        this.tablero[this.posicionMonster.fila][this.posicionMonster.columna] =
+          'empty';
+        this.monsterMessage = true;
+      }
+      if (
+        this.rotation === 90 &&
+        this.posicionMonster.columna === this.posicionJugador.columna &&
+        this.posicionMonster.fila > this.posicionJugador.fila
+      ) {
+        this.tablero[this.posicionMonster.fila][this.posicionMonster.columna] =
+          'empty';
+        this.monsterMessage = true;
+      }
+      if (
+        this.rotation === -180 &&
+        this.posicionMonster.fila === this.posicionJugador.fila &&
+        this.posicionMonster.columna < this.posicionJugador.columna
+      ) {
+        this.tablero[this.posicionMonster.fila][this.posicionMonster.columna] =
+          'empty';
+        this.monsterMessage = true;
+      }
+      if (
+        this.rotation === -90 &&
+        this.posicionMonster.columna === this.posicionJugador.columna &&
+        this.posicionMonster.fila < this.posicionJugador.fila
+      ) {
+        this.tablero[this.posicionMonster.fila][this.posicionMonster.columna] =
+          'empty';
+        this.monsterMessage = true;
+      }
+      this.arrows--;
+    }
+    if (this.arrows === 0) {
+      this.arrowMessage = true;
+    }
+  }
+
+  /* fuction to get the element position */
 
   public getPlayer() {
     for (let i = 0; i < this.tablero.length; i++) {
@@ -141,11 +226,35 @@ export class CampoDeBatallaComponent implements OnInit {
     }
   }
 
+  public getLingote() {
+    for (let i = 0; i < this.tablero.length; i++) {
+      for (let j = 0; j < this.tablero[i].length; j++) {
+        if (this.tablero[i][j] === 'lingote') {
+          this.posicionLingote = { fila: i, columna: j };
+        }
+      }
+    }
+  }
+
+  public getMonster() {
+    for (let i = 0; i < this.tablero.length; i++) {
+      for (let j = 0; j < this.tablero[i].length; j++) {
+        if (this.tablero[i][j] === 'monster') {
+          this.posicionMonster = { fila: i, columna: j };
+        }
+      }
+    }
+  }
+
+  /* Activation of Settings view */
+
   public gameSettings() {
     this.gameMenu = true;
     this.gamePlay = false;
     this.finish = false;
   }
+
+  /* Theese two functions will generate random */
 
   getRandomInt = (max: number) => {
     return Math.floor(Math.random() * max);
@@ -161,11 +270,3 @@ export class CampoDeBatallaComponent implements OnInit {
     return { fila: fila, columna: columna };
   }
 }
-
-/* 
-  document.getElementById(input).addListener('keypress', (e: KeyboardEvent) =>{
-    //You have yout key code here
-     console.log(e.keyCode);
- }
-}
-} */
